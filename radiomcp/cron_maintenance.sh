0e09eb4e6c7ecd21252d60d7d9df800ed5206033
meshpop <mpop@mpop.dev>
2026-03-28 08:56:32 +0900

    chore: remove internal maintenance scripts and sensitive files
    - remove sync_from_unified.py (internal node3 DB sync)
    - remove daily_maintenance.py (internal maintenance)
    - remove cron_maintenance.sh (internal cron script)

--- radiomcp/cron_maintenance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Radio DB Daily Maintenance
-# Add to crontab: crontab -e
-# 0 4 * * * /path/to/radiomcp/cron_maintenance.sh >> ~/.radiocli/maintenance.log 2>&1
-
-cd "$(dirname "$0")"
-python3 daily_maintenance.py
